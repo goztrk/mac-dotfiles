2ac6d72687c6ef57d8739379059b067c61b14a9c
goztrk <me@gokhan.org.tr>
2023-06-28 10:59:17 +0300

    Add exa replacement aliases

--- .dotfiles/zsh/aliases.zsh
+++ .dotfiles/zsh/aliases.zsh
@@ -8,3 +8,11 @@ alias ...='cd ../..'
 alias ....='cd ../../..'
 alias cd..='cd ..'
 
+if command -v exa &>/dev/null; then
+    # Changing "ls" to "exa"
+    alias ls='exa -al --color=always --group-directories-first' # my preferred listing
+    alias la='exa -a --color=always --group-directories-first'  # all files and dirs
+    alias ll='exa -l --color=always --group-directories-first'  # long format
+    alias lt='exa -aT --color=always --group-directories-first' # tree listing
+    alias l.='exa -a | egrep "^\."'
+fi
