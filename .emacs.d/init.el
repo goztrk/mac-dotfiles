5b09a273c39ec771edc295e994fb3da98461e7e4
Gökhan Öztürk <1487670+goztrk@users.noreply.github.com>
2023-08-11 15:10:42 +0300

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,21 @@
+;--- Global Constants ----------------------------------------------------------
+(defconst IS-MAC      (eq system-type 'darwin))
+(defconst IS-LINUX    (memq system-type '(gnu gnu/linux gnu/kfreebsd berkeley-unix)))
+(defconst IS-WINDOWS  (memq system-type '(cygwin windows-nt ms-dos)))
+(defconst IS-BSD      (memq system-type '(darwin berkeley-unix gnu/kfreebsd)))
+;-------------------------------------------------------------------------------
+
+;--- Initial Fixes -------------------------------------------------------------
+;;; Fix $HOME on Windows
+;; $HOME isn't normally defined on Windows, but many unix tools expect it.
+(when IS-WINDOWS
+  (when-let (realhome
+             (and (null (getenv-internal "HOME"))
+                  (getenv "USERPROFILE")))
+    (setenv "HOME" realhome)
+    (setq abbreviated-home-dir nil)))
+;-------------------------------------------------------------------------------
+
 ;--- Bootstrap Straight.el Package Manager --------------------------------------
 ; Source: https://github.com/radian-software/straight.el
 ; Desc:   I think this is better than MELPA because you can pin the commit hash
@@ -31,7 +49,21 @@
   :init
   (setq evil-want-integration t)  ; This is optional since default is `t`
   (setq evil-want-keybinding nil)
-  :config (evil-mode 1))
+  :config (evil-mode 1)
+  :demand t
+  :preface
+  (setq evil-ex-search-vim-style-regexp t
+	evil-ex-visual-char-range t  ; custom range for ex commands
+	evil-mode-line-format 'nil
+	;; More vim like behavior
+	evil-symbol-word-search t
+	;; Only do highlighting in selected window so that Emacs has less work
+        ;; to do highlighting them all.
+        evil-ex-interactive-search-highlight 'selected-window
+	;; It's infuriating that innocuous "beginning of line" or "end of line"
+        ;; errors will abort macros, so suppress them:
+        evil-kbd-macro-suppress-motion-error t
+        evil-undo-system 'undo-redo))
 
 (use-package evil-collection
   :straight t
@@ -71,6 +103,19 @@
   (doom-modeline-mode 1))
 ;-------------------------------------------------------------------------------
 
+;--- Editor Settings -----------------------------------------------------------
+(setq auto-mode-alist
+      (append
+       '(
+	 ("\\.js$" . js-jsx-mode)
+	 ("\\.jsx$" . js-jsx-mode)
+	 ("\\.ts$" . typescript-mode)
+	 ("\\.tsx$" . typescript-mode))
+       auto-mode-alist))
+
+(autoload 'typescript-mode "typescript-mode" "TypeScript mode" t)
+;-------------------------------------------------------------------------------
+
 ;--- Editorconfig --------------------------------------------------------------
 (use-package editorconfig
   :straight t
@@ -78,3 +123,33 @@
   (editorconfig-mode 1))
 ;-------------------------------------------------------------------------------
 
+;--- Python --------------------------------------------------------------------
+(use-package py-isort
+  :straight t
+  :hook
+  (before-save-hook . py-isort-before-save))
+
+(use-package blacken
+  :straight t
+  :hook
+  (python-mode-hook . blacken-mode))
+;-------------------------------------------------------------------------------
+
+;--- JavaScript/TypeScript -----------------------------------------------------
+(use-package prettier-js
+  :straight t
+  :hook
+  (js-jsx-mode-hook . prettier-js-mode)
+  (js2-mode-hook . prettier-js-mode)
+  (typescript-mode-hook . prettier-js-mode)
+  (web-mode-hook . prettier-js-mode))
+
+(use-package typescript-mode
+  :straight t)
+;-------------------------------------------------------------------------------
+
+;--- Tree-Sitter ---------------------------------------------------------------
+(setq treesit-language-source-alist
+      '((tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
+	(python "https://github.com/tree-sitter/tree-sitter-python")))
+;-------------------------------------------------------------------------------
